6d95ee1f50e0785220b1c95e31a5d676241552e9
Yoimithrandir <Yoimithrandir@gmail.com>
2026-08-01 19:28:59 +0800

    manage dataset

---
+++ datasets/imagenet_dataset.py
@@ -0,0 +1,65 @@
+from torchvision import transforms,datasets
+from torch.utils.data import DataLoader
+import random
+
+seed=45
+random.seed(seed)
+torch.manual_seed(seed)
+
+imagenet_root='./data/imagenet'
+imagenet_train_trans=transform.Compose([
+    ResizeShorter,
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomCrop(224),
+
+])
+
+imagenet_val_trans=transform.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize(
+        mean=[0.485, 0.456, 0.406],
+        std=[0.229, 0.224, 0.225]
+    )
+])
+
+class PerPixelMean:
+    def __init__(self):
+        pass
+    def __call__(self,img):
+        pass
+
+class ColourAugmentation:
+    def __init__(self):
+        pass
+    def __call__(self,img):
+        pass
+    
+#resize图像，使短边随机到[256,480]
+class ResizeShorter:
+    def __init__(self,min_size=256,max_size=480):
+        self.min_size=min_size
+        self.max_size=max_size
+
+    def __call__(self,img):
+        size=random.randint(self.min_size,self.max_size)
+        return transforms.Resize(size)(img)
+
+#return train_loader and val_loader
+def get_dataloader(data_root:str,train_trans,val_trans,batch_size=256,num_workers=8):
+    train_path=data_root+'/train'
+    val_path=data_root+'/val'
+    
+    train_dataset=datasets.ImageFolder(train_path,train_trans)
+    val_dataset=datasets.ImageFolder(val_path,val_trans)
+
+    train_loader=DataLoader(train_dataset,batch_size=batch_size,shuffle=True,num_workers=num_workers)
+    val_loader=DataLoader(val_dataset,batch_size=batch_size,shuffle=False,num_workers=num_workers)
+
+    return train_loader,val_loader
+
+
+
+
+
