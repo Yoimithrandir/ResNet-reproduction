d3cb34d81b7ac53a732b05e95928ffd14c16daff
Jacob <Yoimithrandir@gmail.com>
2026-08-02 09:54:50 +0800

    create dataloader branch

--- datasets/dataloader.py
+++ datasets/dataloader.py
@@ -1,20 +1,24 @@
 from torchvision import transforms,datasets
 from torch.utils.data import DataLoader
 import random
+import torch
 
 seed=45
 random.seed(seed)
 torch.manual_seed(seed)
 
 imagenet_root='./data/imagenet'
-imagenet_train_trans=transform.Compose([
-    ResizeShorter,
+imagenet_train_trans=transforms.Compose([
+    transforms.RandomResizedCrop(224),
     transforms.RandomHorizontalFlip(),
-    transforms.RandomCrop(224),
-
+    transforms.ToTensor(),
+    transforms.Normalize(
+        mean=[0.485, 0.456, 0.406],
+        std=[0.229, 0.224, 0.225]
+    )
 ])
 
-imagenet_val_trans=transform.Compose([
+imagenet_val_trans=transforms.Compose([
     transforms.Resize(256),
     transforms.CenterCrop(224),
     transforms.ToTensor(),
@@ -24,18 +28,8 @@ imagenet_val_trans=transform.Compose([
     )
 ])
 
-class PerPixelMean:
-    def __init__(self):
-        pass
-    def __call__(self,img):
-        pass
-
-class ColourAugmentation:
-    def __init__(self):
-        pass
-    def __call__(self,img):
-        pass
-    
+#ResNet的原数据增强方法其一，用于替换RandomResizedCrop
+#scale augmentation引用自论文[41]    
 #resize图像，使短边随机到[256,480]
 class ResizeShorter:
     def __init__(self,min_size=256,max_size=480):
@@ -58,8 +52,5 @@ def get_dataloader(data_root:str,train_trans,val_trans,batch_size=256,num_worker
     val_loader=DataLoader(val_dataset,batch_size=batch_size,shuffle=False,num_workers=num_workers)
 
     return train_loader,val_loader
-
-
-
 
 
